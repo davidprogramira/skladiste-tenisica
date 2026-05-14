0ff0d6a1b8174264e8bac50cd2392b960e2cab77
david blatancic <david.blatancic0@gmail.com>
2026-05-14 10:37:25 +0200

    Add files via upload

---
+++ funkcije.h
@@ -0,0 +1,13 @@
+#ifndef FUNKCIJE_H
+#define FUNKCIJE_H
+
+#include "tenisica.h"
+
+void dodajTenisicu(Tenisica** tenisice, int* n, int* kapacitet);
+void ispisTenisica(Tenisica* tenisice, int n);
+void urediTenisicu(Tenisica* tenisice, int n);
+void obrisiTenisicu(Tenisica** tenisice, int* n);
+void pretraziTenisice(Tenisica* tenisice, int n);
+void sortirajTenisice(Tenisica* tenisice, int n);
+
+#endif
---
+++ main.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <stdlib.h>
+#include "tenisica.h"
+#include "funkcije.h"
+
+int main() {
+    int izbor;
+
+    int n = 0;
+    int kapacitet = 10;
+    Tenisica* tenisice = (Tenisica*)malloc(kapacitet * sizeof(Tenisica));
+
+    if (tenisice == NULL) {
+        perror("Greska pri alokaciji memorije");
+        return 1;
+    }
+    do {
+        printf("\n SKLADISTE TENISICA \n");
+        printf("1. Dodaj tenisicu\n");
+        printf("2. Ispis svih tenisica\n");
+        printf("3. Uredi tenisicu\n");
+        printf("4. Obrisi tenisicu\n");
+        printf("5. Pretrazi tenisice\n");
+        printf("6. Sortiraj tenisice\n");
+        printf("7. Spremi u datoteku\n");
+        printf("8. Ucitaj iz datoteke\n");
+        printf("0. Izlaz\n");
+        printf("Odabir: ");
+        scanf("%d", &izbor);
+
+
+    switch ((OpcijaProg)izbor) {
+        case DODAJ:    dodajTenisicu(&tenisice, &n, &kapacitet);  break;
+        case ISPIS:    ispisTenisica(tenisice, n);                break;
+        case UREDI:    urediTenisicu(tenisice, n);                break;
+        case OBRISI:   obrisiTenisicu(&tenisice, &n);             break;
+        case PRETRAZI: pretraziTenisice(tenisice, n);             break;
+        case SORTIRAJ: sortirajTenisice(tenisice, n);             break;
+        case SPREMI:   printf("Spremanje...\n");                  break;
+        case UCITAJ:   printf("Ucitavanje...\n");                 break;
+        case IZLAZ:    printf("Izlaz...\n");                      break;
+        default:       printf("Nepostojeca opcija!\n");
+        }
+    } while (izbor != 0);
+
+    free(tenisice);
+    tenisice = NULL;
+    return 0;
+}
